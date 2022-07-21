378bb3f3e09e78a4b5a2f5fc754beeaf88c9e351
Doan Thi Lan Anh <doanthilananh001@gmail.com>
2022-07-21 23:07:53 +0700

    User Interaction - Part II

--- src/webdriver/Topic_00_Template.java
+++ src/webdriver/Topic_00_Template.java
@@ -1,12 +1,15 @@
 package webdriver;
 
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -18,6 +21,8 @@ public class Topic_00_Template {
 	WebDriverWait expliciWait;
 	String projectPath = System.getProperty("user.dir");
 	JavascriptExecutor jsExecutor;
+	//Thư viện Actions như thư viện Select
+	Actions actions;
 
 	@BeforeClass
 	public void beforeClass() {
@@ -27,6 +32,8 @@ public class Topic_00_Template {
 		//EXPLIVIWAIT ĐỂ wait các Element theo điều kiện có sẵn
 		expliciWait = new WebDriverWait(driver, 15);
 		jsExecutor = (JavascriptExecutor) driver;
+		//Khai báo trong Before class vì nó khởi tạo cùng lúc với Element
+		actions = new Actions(driver);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("");
